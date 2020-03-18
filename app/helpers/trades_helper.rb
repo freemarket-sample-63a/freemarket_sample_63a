a48759433568d758063e5da4a875ebacd4b32b1f
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-18 21:04:02 +0900

    add trade_submited check
    We coped so that we could not buy product if we had already purchased.

--- app/helpers/trades_helper.rb
+++ app/helpers/trades_helper.rb
@@ -4,7 +4,6 @@ module TradesHelper
   def card_present_and_addres_present?
     @default_card_information.present? && @address.present?
   end
-  
   # 売約済みチェック（tradeレコードがあれば売約済み）
   def trade_submited?
     @trade.present?
