2eeba140bcb6621353a242a04b191f4e64c76f3b
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-21 18:52:44 +0900

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,9 +5,14 @@ FactoryBot.define do
     sequence(:title) { |i| "product_#{i}"}
     sequence(:description) { |i| "description_#{i}"}
 
+    user_id          {1}
+    address_id       {1}
     brand_id         {1}
+    category_id      {1}
     product_size_id  {1}
     shippingway_id   {3}
+    condition_num    {1}
+    daystoship_num   {1}
     price            {1000.000}
     feerate          {0.100}
     profit_price     {900.000}
