55e74ae186213780f8e63846e4a0c37f45decf4e
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-21 20:51:26 +0900

    Update item-image-container.js

--- app/assets/javascripts/item-image-container.js
+++ app/assets/javascripts/item-image-container.js
@@ -17,16 +17,19 @@ $(document).on('turbolinks:load', function() {
     return html
   };
 
+
   //DataTransferオブジェクトで、データを格納する箱を作る
   var dataBox = new DataTransfer();
   //querySelectorでfile_fieldを取得
   var file_field = document.querySelector('input[type=file]')
+  var files_array = [];
    //fileが選択された時に発火するイベント
+
    $('#image-label').change(function(){
     //選択したfileのオブジェクトをpropで取得
     var files = $('input[type="file"]').prop('files')[0];
 
-    $.each(this.files, function(i, file){
+    $.each(files, function(i, file){
       //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
       var fileReader = new FileReader();
       //DataTransferオブジェクトに対して、fileを追加
@@ -47,8 +50,9 @@ $(document).on('turbolinks:load', function() {
         return false;
       }
     });
+    $('#d-d-delete').css('display','none')
+    $('#click-delete').css('display','none');
   });
-  var files_array = [];
 
   $('.item-image-container__unit--guide').on('dragover',function(e){
       e.preventDefault();
@@ -56,26 +60,28 @@ $(document).on('turbolinks:load', function() {
 
   $('.item-image-container__unit--guide').on('drop',function(event){
     event.preventDefault();
-      
-    files = event.originalEvent.dataTransfer.files;
+    var files = event.originalEvent.dataTransfer.files;
 
-    var count = files.length;
+    $.each(files, function(i, file){
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+      //DataTransferオブジェクトに対して、fileを追加
+      dataBox.items.add(file)
+      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
+      file_field.files = dataBox.files
+      var num = $('.item-image-container__unit--preview').length + 1 + i
+      fileReader.readAsDataURL(file);
 
-    if(count<=5){
-      for (var i=0; i<count; i++) {
-        files_array.push(files[i]);
-        var fileReader = new FileReader();
-        fileReader.onload = function( event ) {
-
-        var loadedImageUri = event.target.result;
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var loadedImageUri = fileReader.result
         var html = buildImage(loadedImageUri,i)
         $(html).appendTo(".item-image-container__unit ul").trigger("build");
-        };
-
-        fileReader.readAsDataURL(files[i]);
-
+      };
+      if(dataBox.items.length > 4){
+        return false;
       }
-    }
+    });
     $('#d-d-delete').css('display','none')
     $('#click-delete').css('display','none');
   });
@@ -86,6 +92,10 @@ $(document).on('turbolinks:load', function() {
     files_array.splice(index - 1, 1);
     $(this).parent().parent().parent().remove();
     dataBox.items.remove(dataBox.items[i])
+    if(dataBox.items.length == 0){
+      $('#d-d-delete').css('display','block')
+      $('#click-delete').css('display','block');
+    }
   });
 
   //ここからカテゴリの段階的表示機能
