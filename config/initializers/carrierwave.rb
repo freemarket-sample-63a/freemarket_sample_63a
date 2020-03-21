1b8a590caa4c688fb75d971803949aed648cb19c
mayusee <57439573+mayusee@users.noreply.github.com>
2020-03-21 15:44:25 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,21 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-# S3設定完了までコメントアウト
-#   if Rails.env.production? # 本番環境:AWS
-#     config.storage = :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-#       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-#       region: 'ap-northeast-1'
-#     }
-#   else
-config.storage = :file
-config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-#   end
-
-#   config.fog_directory  = 'finalstage'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/finalstage'
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'finalstage'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/finalstage'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
+  else
+    config.storage = :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
