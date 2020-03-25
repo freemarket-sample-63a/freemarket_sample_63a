5bf45afadb1ac75a28dd056cb38977cba904e0c8
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-25 18:44:21 +0900

    Update trades.rb
    Since it became unnecessary by using association, it was deleted.

--- spec/factories/trades.rb
+++ spec/factories/trades.rb
@@ -3,9 +3,6 @@ FactoryBot.define do
   factory :trade do
 
     status_num  {0}
-    item_id     {1}
-    user_id     {2}
-    address_id  {2}
 
     association :item, factory: :sellitem
     association :address, factory: :buyaddress
