36205cf35361269a2f56e3f16e9699125cce823c
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-25 22:45:42 +0900

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,10 +5,6 @@ FactoryBot.define do
     sequence(:title) { |i| "product_#{i}"}
     sequence(:description) { |i| "description_#{i}"}
 
-    user_id          {1}
-    address_id       {1}
-    brand_id         {1}
-    category_id      {1}
     product_size_id  {1}
     shippingway_id   {3}
     condition_num    {1}
