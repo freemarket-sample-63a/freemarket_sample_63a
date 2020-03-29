45a74a809bb6364a7fca451a3425de3a6af133d6
TomoM8000 <58035988+TomoM8000@users.noreply.github.com>
2020-03-29 16:19:33 +0900

    set01

--- app/models/address.rb
+++ app/models/address.rb
@@ -16,7 +16,6 @@ class Address < ApplicationRecord
     validates :first_name_kana, :last_name_kana, presence: true, length: { maximum: 20}, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力してください。'}
     validates :postal_number, presence: true, length: { is:7 }
     validates :area_id, :city, :number, presence: true
-    # 電話番号は、任意であるが、入力した際には10桁もしくは11桁でしか入力できないようにしたい。
     validates :telephone_number, format:{ with: VALID_PHONE_REGEX, allow_blank: true, message: 'は有効ではありません。'}
 
 
