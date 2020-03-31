acded3d71f81ec3f7cac7966105f7b6736686093
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-31 21:18:56 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 
 
   has_many      :item_images , dependent: :destroy
-  accepts_nested_attributes_for      :item_images
+  accepts_nested_attributes_for      :item_images,allow_destroy: true
 
   belongs_to    :user
   belongs_to    :address
