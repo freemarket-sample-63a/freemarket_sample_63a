52e15e1412a13a30ac0c6da373ab3016e8f9a99d
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-04-03 21:05:59 +0900

    Update addresses_controller.rb
    Corrected the method name to make it easier to understand.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,7 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: [:edit, :update, :destroy]
-  before_action :must_not_destroy_children_exists, only: [:destroy]
-  before_action :must_not_update_trades_exists, only: [:edit]
+  before_action :address_children_exists?, only: [:destroy]
+  before_action :trades_exists?, only: [:edit]
 
   def index
     @addresses = Address.where(user_id: current_user.id)
@@ -18,7 +18,7 @@ class AddressesController < ApplicationController
   end
 
   def edit
-    if must_not_update_trades_exists
+    if trades_exists?
       redirect_to user_addresses_path, notice: "購入に利用されている住所のため変更できません"
     end
   end
@@ -29,7 +29,7 @@ class AddressesController < ApplicationController
   end
 
   def destroy
-    if must_not_destroy_children_exists
+    if address_children_exists?
       redirect_to user_addresses_path, notice:"出品または購入に利用した住所は削除できません" 
     else
       render :index and return unless @address.destroy
@@ -47,12 +47,12 @@ class AddressesController < ApplicationController
     @address = Address.find(params[:id])
   end
 
-  def must_not_destroy_children_exists
+  def address_children_exists?
     #出品済または購入済ならtrueを返す 
     return @address.items.present? || @address.trades.present?  
   end
 
-  def must_not_update_trades_exists
+  def trades_exists?
     #購入済ならtrueを返す 
     item_ids = @address.items.ids
     return Trade.find_by(item_id: item_ids).present?
