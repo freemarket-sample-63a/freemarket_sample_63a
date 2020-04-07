6522944f9f506f1b8a5cff949259a9c9788a90c9
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-04-07 22:16:01 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,122 +2,98 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe '#create' do
+    let(:item) {build_stubbed(:item)}
     context 'can save' do
-
       it "is valid item" do
-        item = build(:item)
         expect(item).to be_valid
       end
-
-      it 'is valid with user_id' do
-        user = create(:user)
-        item = build(:item, user_id: user.id)
-        expect(item).to be_valid
-      end
-
-      it 'is valid with title' do
-        expect(build(:item, title: "product")).to be_valid
-      end
-
-      it 'is valid with description' do
-        expect(build(:item, description: "description")).to be_valid
-      end
-
-      it 'is valid with address' do
-        address = create(:address)
-        item = build(:item, address_id: address.id)
-        expect(item).to be_valid
-      end
-
+      #bland_idはnullでも登録できる
       it 'is valid with brand' do
-        expect(build(:item, brand_id: nil)).to be_valid
-      end
-
-      it 'is valid with category' do
-        category = create(:category)
-        item = build(:item, category_id: category.id)
+        item.brand_id = nil
         expect(item).to be_valid
       end
-
+      #product_sizeはnullでも登録できる
       it 'is valid with product_size' do
-        expect(build(:item, product_size_id: nil)).to be_valid
-      end
-
-      it 'is valid with shippingway' do
-        expect(build(:item, shippingway_id: nil)).to be_valid
-      end
-
-      it 'is valid with condition_num' do
-        expect(build(:item, condition_num: 1)).to be_valid
-      end
-
-      it 'is valid with daystoship_num' do
-        expect(build(:item, daystoship_num: 1)).to be_valid
-      end
-
-      it 'is valid with price' do
-        expect(build(:item, price: 1200.000)).to be_valid
-      end
-
-      it 'is valid with feerate' do
-        expect(build(:item, feerate: 0.100)).to be_valid
-      end
-
-      it 'is valid with profit_price' do
-        expect(build(:item, profit_price: 900.000)).to be_valid
+        item.product_size_id = nil
+        expect(item).to be_valid
       end
     end
 
     context 'can not save' do
-
-      it 'is invalid with user_id' do
-        item = build(:item, user_id: nil)
-        item.valid?
+    #必須事項がnilの場合は登録できない
+      it 'is invalid without user_id' do
+        item.user_id = nil
+        expect(item.errors[:user_id]).to include("を入力してください") if item.valid?
       end
 
       it "is invalid without title" do
-        item = build(:item, title: nil)
-        item.valid?
+        item.title = nil
+        expect(item.errors[:title]).to include("を入力してください") if item.valid?
       end
 
       it "is invalid without description" do
-        item = build(:item, description: nil)
-        item.valid?
+        item.description = nil
+        expect(item.errors[:description]).to include("を入力してください") if item.valid?
       end
 
       it "is invalid without price" do
-        item = build(:item, price: nil)
-        item.valid?
+        item.price = nil
+        expect(item.errors[:price]).to include("を入力してください") if item.valid?
       end
 
-      it 'is invalid with address' do
-        item = build(:item, address_id: nil)
-        item.valid?
+      it 'is invalid without address' do
+        item.address_id = nil
+        expect(item.errors[:address_id]).to include("を入力してください") if item.valid?
       end
 
-      it 'is invalid with category' do
-        item = build(:item, category_id: nil)
-        item.valid?
+      it 'is invalid without category' do
+        item.category = nil
+        expect(item.errors[:category]).to include("を入力してください") if item.valid?
       end
 
-      it 'is invalid with condition_num' do
-        item = build(:item, condition_num: nil)
-        item.valid?
+      it 'is invalid without condition_num' do
+        item.condition_num = nil
+        expect(item.errors[:condition_num]).to include("を入力してください") if item.valid?
       end
 
-      it 'is valid with daystoship_num' do
-        item = build(:item, daystoship_num: nil)
-        item.valid?
+      it 'is invalid without daystoship_num' do
+        item.daystoship_num = nil
+        expect(item.errors[:daystoship_num]).to include("を入力してください") if item.valid?
       end
 
-      it 'is valid with feerate' do
-        item = build(:item, feerate: nil)
-        item.valid?
+      it 'is invalid without feerate' do
+        item.feerate = nil
+        expect(item.errors[:feerate]).to include("を入力してください") if item.valid?
       end
 
-      it 'is valid with profit_price' do
-        item = build(:item, profit_price: nil)
-        item.valid?
+      it 'is invalid without profit_price' do
+        item.profit_price = nil
+        expect(item.errors[:profit_price]).to include("を入力してください") if item.valid?
+      end
+      #priceが200未満の場合は登録できない
+      it 'is invalid price with less than 200' do
+        item.price = 199
+        expect(item).to be_invalid
+      end
+      #priceが10,000,000以上の場合は登録できない
+      it 'is invalid price with more than 10,000,000' do
+        item.price = 10000000
+        expect(item).to be_invalid
+      end
+      #profit_priceが10,000,000以上の場合は登録できない
+      it 'is invalid profit_price with more than 10,000,000' do
+        item.profit_price = 10000000
+        expect(item).to be_invalid
+      end
+      #feerateが1以上の場合は登録できない
+      it 'is invalid feerate more than 1' do
+        item.feerate = 1
+        expect(item).to be_invalid
+      end
+      #feerateが0.001未満の場合は登録できない
+      it 'is invalid feerate less than 0.0009' do
+        item.feerate = 0.0009
+        expect(item).to be_invalid
       end
     end
   end
