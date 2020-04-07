57832812321ae51912aaf9b7a188b39912daa612
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-04-07 22:15:58 +0900

    Update trade_spec.rb

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -2,34 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Trade, type: :model do
   describe "#create" do
-    let(:trade)    { create(:trade) }
+    let(:trade)    {build_stubbed(:trade) }
     it "is invalid without a item_id" do
-      trade = build(:trade, item_id: nil)
-      trade.valid?
-      expect(trade.errors[:item_id]).to include("を入力してください")
+      trade.item_id = nil
+      expect(trade.errors[:item_id]).to include("を入力してください") if trade.valid?
     end
     it "is invalid without a user_id" do
-      trade = build(:trade, user_id: nil)
-      trade.valid?
-      expect(trade.errors[:user_id]).to include("を入力してください")
+      trade.user_id = nil
+      expect(trade.errors[:user_id]).to include("を入力してください") if trade.valid?
     end
     it "is invalid without a address_id" do
-      trade = build(:trade, address_id: nil)
-      trade.valid?
-      expect(trade.errors[:address_id]).to include("を入力してください")
+      trade.address_id = nil
+      expect(trade.errors[:address_id]).to include("を入力してください") if trade.valid?
     end
     it "is invalid without a status_num" do
-      trade = build(:trade, status_num: nil)
-      trade.valid?
-      expect(trade.errors[:status_num]).to include("を入力してください")
+      trade.status_num = nil
+      expect(trade.errors[:status_num]).to include("を入力してください") if trade.valid?
     end
     it "is invalid duplicate a item_id" do
-      trade1 = create(:trade)
-      address2 = create(:buyer_address)
-      user2 = create(:buyer)
-      trade2 = build(:trade, item_id: trade1.item_id, user_id: user2.id, address_id: address2.id)
-      trade2.valid?
-      expect(trade2.errors[:item_id]).to include("はすでに存在します")
+      trade1 = build_stubbed(:trade)
+      address2 = build_stubbed(:buyer_address)
+      user2 = build_stubbed(:buyer)
+      trade2 = build_stubbed(:trade, item_id: trade1.item_id, user_id: user2.id, address_id: address2.id)
+      expect(trade2.errors[:item_id]).to include("はすでに存在します") if trade2.valid?
     end
     it "is valid trade" do
       expect(trade).to be_valid
