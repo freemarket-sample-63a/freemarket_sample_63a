cf888a9ddbcdb91f01d8b94fe1af7db3e016efc5
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-04-11 16:17:45 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,5 @@
 class CategoriesController < ApplicationController
+  skip_before_action :set_header_categories,:set_header_brands
   def index
-    @categories = Category.where(ancestry: params[:id])
-    respond_to do |format|
-      format.json
-    end
   end
 end
