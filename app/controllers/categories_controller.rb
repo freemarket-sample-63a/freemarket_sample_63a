5d46f01ab241f24b5c7ca38717bdd092203944a2
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-04-15 07:02:24 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @items = Item.where(category_id: @category.descendant_ids)
+    if @category.descendant_ids.present?
+      @items = Item.where(category_id: @category.descendant_ids)      
+    else
+      @items = Item.where(category_id: @category.id)  
+    end
   end
 end
