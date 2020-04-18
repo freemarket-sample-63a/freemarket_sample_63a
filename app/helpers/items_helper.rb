0492bc3fa3630ec167e384645d796fc5439da69d
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-04-18 19:30:36 +0900

    Update items_helper.rb

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -8,6 +8,10 @@ module ItemsHelper
     EOS
   end
 
+  def disp_ratepercent(rate)
+    return "#{(rate * 100).to_i}%"
+  end
+
   def thousands_separator(price)
     # 現状はJPY固定。
     number_to_currency(price, format: "%u%n", unit: "￥", strip_insignificant_zeros: true)
